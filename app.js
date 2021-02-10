d064afb04e089b60649d2029b0c61813b0d17158
kami456 <kamranshahid218@gmail.com>
2021-02-10 02:28:07 -0800

    stopwatch

---
+++ app.js
@@ -0,0 +1,53 @@
+var start, stop, stopwatchBox;
+start = document.getElementById("start");
+stop = document.getElementById("stop");
+stopwatchBox = document.getElementById("stopwatch-box");
+start.addEventListener("click", function() {
+    stopwatch("start");
+  });
+  
+  stop.addEventListener("click", function() {
+    stopwatch("stop");
+  });
+  timerId = null;
+  function stopwatch(command) {
+    var hours = 0,
+      minutes = 0,
+      seconds = 0,
+      display;
+  
+    function makeTwoDigits(number) {
+      // display double digits for numbers less than 10
+      if (number < 10) {
+        return "0" + number;
+      }
+  
+      return number;
+    }
+  
+    if (command === "start") {
+      timerId = setInterval(function() {
+        seconds++;
+        if (seconds === 60) {
+          seconds = 0;
+          minutes++;
+        }
+  
+        if (minutes === 60) {
+          minutes = 0;
+          hours++;
+        }
+  
+        display =
+          makeTwoDigits(hours) +
+          ":" +
+          makeTwoDigits(minutes) +
+          ":" +
+          makeTwoDigits(seconds);
+        stopwatchBox.innerHTML = display;
+        console.log(display);
+      }, 1000);
+    } else if (command === "stop") {
+      clearInterval(timerId);
+    }
+  }  
